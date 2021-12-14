97f2647fc3297fdfbbcc6f5a88f4f8832b96b564
orozcoding <orozcoding@gmail.com>
2021-12-14 19:16:14 -0400

    Fix linter reports

--- js/projects.js
+++ js/projects.js
@@ -11,8 +11,8 @@ const project1 = {
   tag2: 'css',
   tag3: 'javaScript',
   btnId: 'work-btn-1',
-  details: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent'
-}
+  details: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent',
+};
 
 const project2 = {
   id: 'work-2',
@@ -27,8 +27,8 @@ const project2 = {
   tag2: 'css',
   tag3: 'javaScript',
   btnId: 'work-btn-2',
-  details: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent'
-}
+  details: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent',
+};
 
 const project3 = {
   id: 'work-3',
@@ -43,8 +43,8 @@ const project3 = {
   tag2: 'css',
   tag3: 'javaScript',
   btnId: 'work-btn-3',
-  details: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent'
-}
+  details: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent',
+};
 
 const project4 = {
   id: 'work-4',
@@ -59,8 +59,8 @@ const project4 = {
   tag2: 'css',
   tag3: 'javaScript',
   btnId: 'work-btn-4',
-  details: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent'
-}
+  details: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essent',
+};
 
 const projects = [project1, project2, project3, project4];
 
@@ -68,38 +68,38 @@ const projects = [project1, project2, project3, project4];
 
 const workSection = document.querySelector('#work-section');
 
-for (i = 0; i < projects.length ; i += 1){
-  let workCard = document.createElement('div');
+for (let i = 0; i < projects.length; i += 1) {
+  const workCard = document.createElement('div');
   workCard.id = projects[i].id;
   workCard.classList.add('work-card', 'd-flex');
 
-  let img = document.createElement('img');
+  const img = document.createElement('img');
   img.src = projects[i].img;
   img.alt = projects[i].alt;
   img.classList.add('card-preview');
 
-  let cardInfo = document.createElement('div');
+  const cardInfo = document.createElement('div');
   cardInfo.classList.add('card-info');
 
-  let workTitle = document.createElement('h3');
+  const workTitle = document.createElement('h3');
   workTitle.classList.add('work-title');
   workTitle.innerHTML = projects[i].title;
 
-  let workInfo = document.createElement('div');
+  const workInfo = document.createElement('div');
   workInfo.classList.add('work-info');
 
-  let client = document.createElement('p');
+  const client = document.createElement('p');
   client.classList.add('client');
   client.innerHTML = projects[i].client;
 
-  let counter = document.createElement('div');
+  const counter = document.createElement('div');
   counter.classList.add('counter');
 
-  let job = document.createElement('p');
+  const job = document.createElement('p');
   job.classList.add('job');
   job.innerHTML = projects[i].job;
 
-  let year = document.createElement('p');
+  const year = document.createElement('p');
   year.classList.add('year');
   year.innerHTML = projects[i].year;
 
@@ -109,27 +109,27 @@ for (i = 0; i < projects.length ; i += 1){
   workInfo.appendChild(counter);
   workInfo.appendChild(year);
 
-  let workDescription = document.createElement('p');
+  const workDescription = document.createElement('p');
   workDescription.classList.add('work-description');
   workDescription.innerHTML = projects[i].description;
 
-  let workTags = document.createElement('ul');
+  const workTags = document.createElement('ul');
   workTags.classList.add('work-tags');
 
-  let htmlTag = document.createElement('li');
-  let htmlBtn = document.createElement('button');
+  const htmlTag = document.createElement('li');
+  const htmlBtn = document.createElement('button');
   htmlBtn.classList.add('tag-button', 'focus');
   htmlBtn.innerHTML = projects[i].tag1;
   htmlTag.appendChild(htmlBtn);
 
-  let cssTag = document.createElement('li');
-  let cssBtn = document.createElement('button');
+  const cssTag = document.createElement('li');
+  const cssBtn = document.createElement('button');
   cssBtn.classList.add('tag-button', 'focus');
   cssBtn.innerHTML = projects[i].tag2;
   cssTag.appendChild(cssBtn);
 
-  let javaScriptTag = document.createElement('li');
-  let javaScriptBtn = document.createElement('button');
+  const javaScriptTag = document.createElement('li');
+  const javaScriptBtn = document.createElement('button');
   javaScriptBtn.classList.add('tag-button', 'focus');
   javaScriptBtn.innerHTML = projects[i].tag3;
   javaScriptTag.appendChild(javaScriptBtn);
@@ -138,7 +138,7 @@ for (i = 0; i < projects.length ; i += 1){
   workTags.appendChild(cssTag);
   workTags.appendChild(javaScriptTag);
 
-  let seeProject = document.createElement('button');
+  const seeProject = document.createElement('button');
   seeProject.type = 'button';
   seeProject.name = 'see-project';
   seeProject.classList.add('work-button', 'focus');
@@ -159,22 +159,31 @@ for (i = 0; i < projects.length ; i += 1){
 
 // modals script
 
-function modalDisplay () {
+let modal;
+let overlay;
+
+function modalClose() {
+  modal.remove();
+  overlay.remove();
+  document.body.classList.remove('no-scroll');
+}
+
+function modalDisplay() {
   let project;
-  if (this.id == project1.btnId) {
+  if (this.id === project1.btnId) {
     project = project1;
-  } else if (this.id == project2.btnId) {
+  } else if (this.id === project2.btnId) {
     project = project2;
-  } else if (this.id == project3.btnId) {
+  } else if (this.id === project3.btnId) {
     project = project3;
-  } else if (this.id == project4.btnId) {
+  } else if (this.id === project4.btnId) {
     project = project4;
   }
 
-  let overlay = document.createElement('div');
+  const overlay = document.createElement('div');
   overlay.id = 'overlay';
 
-  let modal = document.createElement('div');
+  const modal = document.createElement('div');
   modal.id = 'modal';
   modal.className = 'd-flex';
   modal.innerHTML = `
@@ -227,15 +236,8 @@ function modalDisplay () {
   modalCloseBtn.addEventListener('click', modalClose);
 }
 
-function modalClose () {
-  modal.remove();
-  overlay.remove();
-  document.body.classList.remove('no-scroll');
-}
-
 const workBtn = document.querySelectorAll('.work-button');
 
 workBtn.forEach((element) => {
   element.addEventListener('click', modalDisplay);
 });
-
