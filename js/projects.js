04e3297d14b241969b9bea9d7a6e1160be5288b6
joskalenda <joskalenda3@gmail.com>
2021-12-14 19:21:00 +0200

    Creat cards objects

---
+++ js/projects.js
@@ -0,0 +1,41 @@
+const project1 = {
+  id: 'work-1',
+  alt: 'Work One Preview',
+  title: 'Tonic',
+  client: 'CANOPY',
+  job: 'Back End Dev',
+  year: '2015',
+  img: 'img/works/work1.png',
+  description: 'A daily selection of privately personalized reads; no accounts or sign-ups required.',
+  tag1: 'html',
+  tag2: 'css',
+  tag3: 'javaScript'
+}
+
+const project2 = {
+  id: 'work-2',
+  alt: 'Work Two Preview',
+  title: 'Multi-Post Stories',
+  client: 'CANOPY',
+  job: 'Back End Dev',
+  year: '2015',
+  img: 'img/works/work2.png',
+  description: 'A daily selection of privately personalized reads; no accounts or sign-ups required.',
+  tag1: 'html',
+  tag2: 'css',
+  tag3: 'javaScript'
+}
+
+const project3 = {
+  id: 'work-3',
+  alt: 'Work Three Preview',
+  title: 'Tonic',
+  client: 'CANOPY',
+  job: 'Back End Dev',
+  year: '2015',
+  img: 'img/works/work3.png',
+  description: 'A daily selection of privately personalized reads; no accounts or sign-ups required.',
+  tag1: 'html',
+  tag2: 'css',
+  tag3: 'javaScript'
+}
