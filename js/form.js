42d445e6dc9fb249488b3a5da8cab4f26bcdb470
orozcoding <orozcoding@gmail.com>
2021-12-15 10:21:20 -0400

    JS → Create script - email validation

---
+++ js/form.js
@@ -0,0 +1,15 @@
+const email = document.getElementById('form-email');
+const errorMsg = document.getElementsByClassName('form-error');
+const form = document.getElementById('form');
+
+form.addEventListener('submit', (event) =>{
+
+  if (email.value !== email.value.toLowerCase()) {
+    event.preventDefault();
+    errorMsg[1].innerHTML = 'Please use lower case.';
+    email.style.border = 'solid 5px red';
+  } else {
+    errorMsg[1].innerHTML = '';
+    email.style.border = null;
+  }
+});
