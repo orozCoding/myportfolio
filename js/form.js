487c7adc07ec5545c96e4d05679e37330fcc499f
orozcoding <orozcoding@gmail.com>
2021-12-16 03:04:53 -0400

    Finish solo-storage form

--- js/form.js
+++ js/form.js
@@ -1,14 +1,54 @@
-const email = document.getElementById('form-email');
+const nameInput = document.getElementById('form-name');
+const emailInput = document.getElementById('form-email');
+const msgInput = document.getElementById('form-msg');
 const errorMsg = document.getElementsByClassName('form-error');
 const form = document.getElementById('form');
 
 form.addEventListener('submit', (event) => {
-  if (email.value !== email.value.toLowerCase()) {
+  if (emailInput.value !== emailInput.value.toLowerCase()) {
     event.preventDefault();
     errorMsg[1].innerHTML = 'Please use lower case.';
-    email.style.border = 'solid 5px red';
+    emailInput.style.border = 'solid 5px red';
   } else {
     errorMsg[1].innerHTML = '';
-    email.style.border = null;
+    emailInput.style.border = null;
   }
 });
+
+// new code
+
+let formStorage = {
+  formName: '',
+  formEmail: '',
+  formMsg: ''
+}
+
+getLocalStorage();
+
+localStorage.setItem('formInfo', JSON.stringify( formStorage ));
+
+nameInput.addEventListener('change', (event) => {
+  formStorage.formName = nameInput.value;
+  localStorage.setItem('formInfo', JSON.stringify( formStorage ));
+})
+
+emailInput.addEventListener('change', (event) => {
+  formStorage.formEmail = emailInput.value;
+  localStorage.setItem('formInfo', JSON.stringify( formStorage ));
+})
+
+msgInput.addEventListener('change', (event) => {
+  formStorage.formMsg = msgInput.value;
+  localStorage.setItem('formInfo', JSON.stringify( formStorage ));
+})
+
+function getLocalStorage () {
+
+  if (localStorage.getItem('formInfo')) {
+    formStorage = JSON.parse(localStorage.getItem('formInfo'));
+    nameInput.value = formStorage.formName;
+    emailInput.value = formStorage.formEmail;
+    msgInput.value = formStorage.formMsg;
+  }
+
+}
