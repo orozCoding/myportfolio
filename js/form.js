80689199f6c417fd46a4c19edecbca85fda7c96e
orozcoding <orozcoding@gmail.com>
2021-12-16 04:54:02 -0400

    form.js → create script for localStorage

--- js/form.js
+++ js/form.js
@@ -18,3 +18,33 @@ let formStorage = {
   formEmail: '',
   formMsg: ''
 }
+
+getLocalStorage();
+
+localStorage.setItem('formInfo', JSON.stringify( formStorage ));
+
+nameInput.addEventListener('change', (event) => {
+  formStorage.formName = nameInput.value;
+  localStorage.setItem('formInfo', JSON.stringify( formStorage ));
+})
+
+emailInput.addEventListener('change', (event) => {
+  formStorage.formEmail = emailInput.value;
+  localStorage.setItem('formInfo', JSON.stringify( formStorage ));
+})
+
+msgInput.addEventListener('change', (event) => {
+  formStorage.formMsg = msgInput.value;
+  localStorage.setItem('formInfo', JSON.stringify( formStorage ));
+})
+
+function getLocalStorage () {
+
+  if (localStorage.getItem('formInfo')) {
+    formStorage = JSON.parse(localStorage.getItem('formInfo'));
+    nameInput.value = formStorage.formName;
+    emailInput.value = formStorage.formEmail;
+    msgInput.value = formStorage.formMsg;
+  }
+
+}
